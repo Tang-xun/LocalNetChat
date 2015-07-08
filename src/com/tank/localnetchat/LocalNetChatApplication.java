e35dfb08daf01485c6ed93db7059fdc964ca0a8a
tangxun <tangxun_123@163.com>
2015-07-08 17:49:08 +0800

    修改Application 文件

--- src/com/tank/localnetchat/LocalNetChatApplication.java
+++ src/com/tank/localnetchat/LocalNetChatApplication.java
@@ -1,5 +1,19 @@
 package com.tank.localnetchat;
 
-public class LocalNetChatApplication {
+import android.app.Application;
 
+/**
+ * 
+ * @author tangxun    E-mail:tangxun325@pingan.com.cn
+ * @version 1.1.0	
+ * @createDate 2015-7-8 下午5:47:21
+ * 
+ */
+public class LocalNetChatApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    
 }
